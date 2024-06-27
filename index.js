b47bcc0ab41980a512f3be5b5a11c436d95524c9
Santiago Zalazar <santiagozalazar1@gmail.com>
2024-06-27 19:32:10 -0300

    Primer Sprint finish (?

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static('./public'));
+
+app.listen(3000, ()=>{
+    console.log('Servidor corriendo en el http://localhost:3000');
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+});
